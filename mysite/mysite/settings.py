f463620a55dc5105f915c2eab11bd99656ec8c10
Sana Javed <sanajaved7@gmail.com>
2015-05-09 10:21:27 -0400

    activated admin site

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    #'django.contrib.admin',
+    'django.contrib.admin',
     #'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from polls.models import Poll
+from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Poll)
