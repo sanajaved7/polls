0df7651830ba0fdb50a2f9816037cc9533d21e61
Sana Javed <sanajaved7@gmail.com>
2015-05-09 11:08:47 -0400

    Added search and filter capability to admin site

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,6 +10,10 @@ class Poll(models.Model):
         return self.question
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    date_hierarchy = 'pub_date'
 
 
 class Choice(models.Model):
